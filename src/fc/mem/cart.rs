6095638df8ab2eae722b8ef0ee1d59ee7918b93d
iskake <iskake@protonmail.com>
2024-12-19 23:02:37 +0100

    Add functions for getting info from the nes file headers

--- src/fc/mem/cart.rs
+++ src/fc/mem/cart.rs
@@ -1,14 +1,17 @@
+// For the actual specifications see the wiki:
+// https://www.nesdev.org/wiki/NES_2.0
+
 use std::{
     fs::File,
     io::{self, Read},
 };
 
-use crate::fc::mem::mapper::MapperType;
+use crate::{bits::Bitwise, fc::mem::mapper::MapperType};
 
 const NES_FILE_IDENTIFIER: [u8; 4] = [b'N', b'E', b'S', 0x1a];
 
 pub struct NESFile {
-    header: NESFileHeader,
+    pub header: NESFileHeader, // ? change this to private?
     pub data: Vec<u8>,
 }
 
@@ -29,6 +32,7 @@ impl NESFile {
             // println!("{}", bytes.len());
             let header = NESFileHeader::from_slice(&bytes.as_slice()[4..16]);
 
+            println!("NES2.0 header? {}", header.is_nes20_format());
             NESFile {
                 header: header,
                 data: data,
@@ -40,61 +44,243 @@ impl NESFile {
     }
 
     pub fn mapper_type(&self) -> MapperType {
-        match self.header.mapper_type() {
+        match self.header.mapper_number() {
             0 => MapperType::NROM,
             i => MapperType::UNKNOWN(i),
         }
     }
 }
 
-// TODO: make an enum for iNES / NES 2.0 formats? or ignore because NES2.0 is backwards compatible?
-struct NESFileHeader {
-    pub prg_rom_size: u8,
-    pub chr_rom_size: u8,
-    pub flags6: u8,
-    pub flags7: u8,
-    pub flags8: u8,
-    pub flags9: u8,
-    pub flags10: u8,
-    // NES 2.0:
-    pub flags_chr_ram_size: u8,
-    pub flags_cpu_ppu_timing: u8,
-    pub flags_vs_system: u8,
-    pub flags_misc_roms: u8,
-    pub flags_default_expansion_device: u8,
+
+pub struct NESFileHeader {
+    prg_rom_size_lsb: u8,
+    chr_rom_size_lsb: u8,
+    flags6: u8,
+    flags7: u8,
+    flags8: u8,
+    flags9: u8,
+    flags10: u8,
+    // NES 2.0 only
+    flags11: u8,
+    flags12: u8,
+    flags13: u8,
+    flags14: u8,
+    flags15: u8,
 }
 
 impl NESFileHeader {
+    /// Construct a `NESFileHeader` from a `&[u8]`
     fn from_slice(bytes: &[u8]) -> NESFileHeader {
         NESFileHeader {
-            prg_rom_size: bytes[0],
-            chr_rom_size: bytes[1],
+            prg_rom_size_lsb: bytes[0],
+            chr_rom_size_lsb: bytes[1],
             flags6: bytes[2],
             flags7: bytes[3],
             flags8: bytes[4],
             flags9: bytes[5],
             flags10: bytes[6],
-            flags_chr_ram_size: bytes[7],
-            flags_cpu_ppu_timing: bytes[8],
-            flags_vs_system: bytes[9],
-            flags_misc_roms: bytes[10],
-            flags_default_expansion_device: bytes[11],
+            flags11: bytes[7],
+            flags12: bytes[8],
+            flags13: bytes[9],
+            flags14: bytes[10],
+            flags15: bytes[11],
         }
     }
 
-    fn is_nes20_format(&self) -> bool {
+    /// Whether the header has the NES2.0 format or not
+    pub fn is_nes20_format(&self) -> bool {
         self.flags7 & 0x0c == 0x08
     }
 
-    pub fn mapper_type(&self) -> u16 {
-        // TODO: nes2.0 headers...
+    // TODO? move these functions to NESFile instead?
+
+    /// Nametable layout according to byte 6 of the header
+    ///
+    /// - `0`: vertical arrangement (horizontal mirror) / mapper controlled
+    /// - `1`: horizontal arrangement (vertical mirror)
+    pub fn nametable_layout(&self) -> bool {
+        self.flags6.test_bit(0)
+    }
+
+    /// Whether a "battery"/non-volatile memory is present according to byte 6 of the header
+    ///
+    /// - `0`: not present
+    /// - `1`: present
+    pub fn battery(&self) -> bool {
+        self.flags6.test_bit(1)
+    }
+
+    /// Whether a 512-bit trainer is present according to byte 6 of the header
+    ///
+    /// - `0`: not present
+    /// - `1`: "present between header and PRG-ROM data"
+    pub fn trainer(&self) -> bool {
+        self.flags6.test_bit(2)
+    }
+
+    /// Whether a 512-bit trainer is present according to byte 6 of the header
+    ///
+    /// - `0`: not present
+    /// - `1`: "present between header and PRG-ROM data"
+    pub fn alt_nametable_layout(&self) -> bool {
+        self.flags6.test_bit(3)
+    }
+
+    /// Get the mapper number stored in byte 6of the header (and bytes 7,8 when using the NES2.0 format)
+    pub fn mapper_number(&self) -> u16 {
         let m0 = (self.flags6 & 0xf0) >> 4;
         if !self.is_nes20_format() {
             m0 as u16
         } else {
             let m1 = (self.flags7 & 0xf0) >> 4;
-            let m2 = (self.flags8 & 0x0f);
+            let m2 = self.flags8 & 0x0f;
             ((m2 as u16) << 8) | ((m1 as u16) << 4) | m0 as u16
+        }
+    }
+
+    /// Get the submapper number stored in byte 7 of the header (NES2.0 only)
+    pub fn submapper_number(&self) -> u8 {
+        if self.is_nes20_format() {
+            (self.flags7 & 0xf0) >> 4
+        } else {
+            0
+        }
+    }
+
+    /// Get the 'type' of console according to byte 7 of the header (and byte 13 if console type is 3)
+    pub fn console_type(&self) -> u8 {
+        let val = self.flags7 & 0b11;
+        if self.is_nes20_format() && val == 3 {
+            // Extended console type
+            ((self.flags13 & 0xf) << 2) | val
+        } else {
+            // Non-extended console type
+            val
+        }
+    }
+
+    /// Get the size of PRG-ROM in bytes
+    pub fn prg_rom_size(&self) -> u64 {
+        if !self.is_nes20_format() {
+            let val = self.prg_rom_size_lsb as u64;
+            val * 0x4000
+        } else {
+            if (self.flags9 as u64 & 0xf) == 0xf {
+                // Exponent-multiplier notation
+                let multiplier = (self.chr_rom_size_lsb & 0b11) as u64;
+                let exponent = ((self.chr_rom_size_lsb & 0b1111_1100) >> 2) as u64;
+                // TODO: check for overflow?
+                (1 << exponent) * (multiplier * 2 + 1)
+            } else {
+                let val = ((self.flags9 as u64 & 0xf) << 4) | self.prg_rom_size_lsb as u64;
+                val * 0x4000
+            }
+        }
+    }
+
+    /// Get the size of CHR-ROM in bytes
+    pub fn chr_rom_size(&self) -> u64 {
+        if !self.is_nes20_format() {
+            let val = self.chr_rom_size_lsb as u64;
+            val
+        } else {
+            if (self.flags9 as u64 & 0xf0) == 0xf0 {
+                // Exponent-multiplier notation
+                let multiplier = (self.chr_rom_size_lsb & 0b11) as u64;
+                let exponent = ((self.chr_rom_size_lsb & 0b1111_1100) >> 2) as u64;
+                // TODO: check for overflow?
+                (1 << exponent) * (multiplier * 2 + 1)
+            } else {
+                // Normal
+                let val = ((self.flags9 as u64 & 0xf0) << 4) | self.chr_rom_size_lsb as u64;
+                val * 0x2000
+            }
+        }
+    }
+
+    /// Get the PRG-RAM (volatile) shift count (NES2.0 only)
+    ///
+    /// - "If the shift count is zero, there is no CHR-(NV)RAM."
+    /// - "If the shift count is non-zero, the actual size is
+    ///   - "64 << shift count" bytes, i.e. 8192 bytes for a shift count of 7."
+    pub fn prg_ram_shift_count(&self) -> u8 {
+        if self.is_nes20_format() {
+            self.flags10 & 0x0f
+        } else {
+            0
+        }
+    }
+
+    /// Get the PRG-NVRAM/EEPROM (non-volatile) shift count (NES2.0 only)
+    ///
+    /// - "If the shift count is zero, there is no CHR-(NV)RAM."
+    /// - "If the shift count is non-zero, the actual size is
+    ///   - "64 << shift count" bytes, i.e. 8192 bytes for a shift count of 7."
+    pub fn prg_nvram_eeprom_shift_count(&self) -> u8 {
+        if self.is_nes20_format() {
+            (self.flags10 & 0xf0) >> 4
+        } else {
+            0
+        }
+    }
+
+    /// Get the CHR-RAM (volatile) shift count (NES2.0 only)
+    ///
+    /// - "If the shift count is zero, there is no CHR-(NV)RAM."
+    /// - "If the shift count is non-zero, the actual size is
+    ///   - "64 << shift count" bytes, i.e. 8192 bytes for a shift count of 7."
+    pub fn chr_ram_shift_count(&self) -> u8 {
+        if self.is_nes20_format() {
+            self.flags11 & 0x0f
+        } else {
+            0
+        }
+    }
+
+    /// Get the CHR-NVRAM (non-volatile) shift count (NES2.0 only)
+    ///
+    /// - "If the shift count is zero, there is no CHR-(NV)RAM."
+    /// - "If the shift count is non-zero, the actual size is
+    ///   - "64 << shift count" bytes, i.e. 8192 bytes for a shift count of 7."
+    pub fn chr_nvram_shift_count(&self) -> u8 {
+        if self.is_nes20_format() {
+            (self.flags11 & 0xf0) >> 4
+        } else {
+            0
+        }
+    }
+
+    /// Get the CPU/PPU timing mode (NES2.0 only)
+    ///
+    /// - 0: RP2C02 ("NTSC NES")
+    /// - 1: RP2C07 ("Licensed PAL NES")
+    /// - 2: Multiple-region
+    /// - 3: UA6538 ("Dendy")
+    pub fn cpu_ppu_timing_mode(&self) -> u8 {
+        if self.is_nes20_format() {
+            self.flags12 & 0b11
+        } else {
+            0
+        }
+    }
+
+    // TODO: vs system type...
+
+    /// Get the number of miscellaneous ROMs present (NES2.0 only)
+    pub fn misc_roms_count(&self) -> u8 {
+        if self.is_nes20_format() {
+            self.flags14 & 0b11
+        } else {
+            0
+        }
+    }
+
+    /// Get the default expansion device (NES2.0 only)
+    pub fn default_expansion_device(&self) -> u8 {
+        if self.is_nes20_format() {
+            self.flags15 & 0x3f
+        } else {
+            0
         }
     }
 }
--- src/fc/mod.rs
+++ src/fc/mod.rs
@@ -7,6 +7,13 @@ pub mod cpu;
 pub mod dbg;
 pub mod mem;
 
+pub enum ConsoleType {
+    Famicom,
+    VSSystem,
+    PlayChoice,
+    Other,
+}
+
 pub struct FC {
     cpu: CPU,
 }
@@ -24,7 +31,7 @@ impl FC {
     }
 
     fn load_rom(&mut self, filename: &str) -> () {
-        // TODO: change this?
+        // TODO? change this so you don't have to create yet another cpu? or is this way better?
         let mut cpu = CPU::new();
         let nesfile = NESFile::from_file(filename).expect(&format!("File not found: {filename}"));
         cpu.mem = MemMap::from_nesfile(nesfile);
