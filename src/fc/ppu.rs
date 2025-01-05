fb1cc903ce2f6b56c4ed0c9f5a980809ad27d344
iskake <iskake@protonmail.com>
2025-01-05 23:34:56 +0100

    Implement ppu rendering At the moment, it's pretty buggy and unfinished, but it can successfully display the title screen of super mario bros. (with a tiny attribute error on the mushroom next to the 1 player game text)

--- src/fc/ppu.rs
+++ src/fc/ppu.rs
@@ -46,6 +46,15 @@ pub struct PPU {
     cycle: u32,
     scanline: u32,
     frame: u64,
+    // rendering
+    curr_tile_idx: u8,
+    prev_attribute_byte: u8,
+    curr_attribute_byte: u8,
+    temp_attribute_byte: u8,
+    curr_pattern_lo: u8,
+    curr_pattern_hi: u8,
+    shift_reg_lo: u16,
+    shift_reg_hi: u16,
     // ??vvv
     frame_buf: Vec<u8>,
     nametable_buf: Vec<u8>,
@@ -63,7 +72,16 @@ impl PPU {
             scanline: 0,
             frame: 0,
             frame_buf: vec![0; PICTURE_HEIGHT * PICTURE_WIDTH * 3],
-            nametable_buf: vec![0; PICTURE_HEIGHT * PICTURE_WIDTH * 4 * 3]
+            nametable_buf: vec![0; PICTURE_HEIGHT * PICTURE_WIDTH * 4 * 3],
+
+            curr_tile_idx: 0,
+            prev_attribute_byte: 0,
+            curr_attribute_byte: 0,
+            temp_attribute_byte: 0,
+            curr_pattern_lo: 0,
+            curr_pattern_hi: 0,
+            shift_reg_lo: 0,
+            shift_reg_hi: 0,
         }
     }
 
@@ -110,7 +128,7 @@ impl PPU {
         self.scanline
     }
 
-    pub(crate) fn is_vblank(&self) -> bool {
+    pub(crate) fn _is_vblank(&self) -> bool {
         self.reg.status.vblank
     }
 
@@ -137,64 +155,7 @@ impl PPU {
             assert!(self.cycle < SCANLINE_DURATION);
             assert!(self.scanline < FRAME_SCANLINES);
 
-            // TODO
-            match self.scanline {
-                0..=239 => {    // rendering (visible scanlines)
-                    // TODO
-                    if !self.reg.mask.bg_enable || true {
-                        // Rendering disabled
-                        let x = self.cycle as usize;
-                        let y = self.scanline as usize;
-                        let idx = y * PICTURE_HEIGHT + x;
-
-                        let px = RGB8 {
-                            r: (self.frame & 0xff) as u8,
-                            g: (self.frame & 0xff) as u8,
-                            b: (self.frame & 0xff) as u8,
-                        };
-
-                        self.frame_buf.as_rgb_mut()[idx] = px;
-                    } else {
-                        match self.cycle {
-                            0 => {},         // idle cycle
-                            1..=256 => {     // vram fetch/update
-                                // Fetch data. Each byte takes 2 cycles
-                                // let c = (self.cycle - 1) % 8;
-                                // dbg!(c);
-                                // match c {
-                                //     1 => {},    // nametable byte
-                                //     3 => {},    // attribute table byte
-                                //     5 => {},    // pattern table tile low
-                                //     7 => {},    // pattern table tile high (+8 bytes from pattern table tile low)
-                                //     _ => {},
-                                // }
-                                if self.cycle == 256 {
-                                    // TODO: actual fetch+write bytes instead of full scanline
-                                    self.add_scanline_temp(mem);
-                                }
-                            },
-                            257..=320 => {}, // fetch tile data for sprites on the next scanline
-                            321..=336 => {}, // fetch tile data for first two tiles for the next scanline
-                            337..=340 => {}, // fetch two bytes
-                            _ => unreachable!(),
-                        }
-                    }
-                },
-                240 => {},      // idle (post-render scanline)
-                241..=260 => {  // vblank
-                    if self.cycle == 1 && self.scanline == 241 {
-                        self.reg.status.vblank = true;
-                        debug!("enabled vblank")
-                    }
-                },
-                261 => {     // dummy scanline (pre-render scanline)
-                    if self.cycle == 1 {
-                        debug!("disabled vblank");
-                        self.reg.status.vblank = false;
-                    }
-                },
-                _ => unreachable!(),
-            }
+            self.render(mem);
 
             self.cycle += 1;
             if self.cycle >= SCANLINE_DURATION {
@@ -296,13 +257,6 @@ impl PPU {
         self.oam[dst as usize] = val;
     }
 
-    fn add_scanline_temp(&self, _mem: &mut MemMap) -> () {
-        for _i in 0..=255 {
-            // todo!();
-            // self.read_addr(0x2000 + i, mem); // ???
-        }
-    }
-
     fn write_ppuctrl(&mut self, val: u8) -> () {
         // TODO: check for cycles < 29658 before allowing writes?
         self.reg.control = val.into();
@@ -349,6 +303,7 @@ impl PPU {
             self.reg.t = self.reg.t & 0x7f00 | (val as u16);
         } else {
             self.reg.t = (((val & 0x7f) as u16) << 8) | self.reg.t & 0xff;
+            self.reg.scroll_x = val & 0x07;
         }
 
         debug!("Wrote ${val:02x} to PPUSCROLL (${ADDRESS_PPUSCROLL:04x}) (lo: {})", self.reg.write_toggle);
@@ -412,6 +367,243 @@ impl PPU {
         &self.frame_buf
     }
 
+    fn rendering_enabled(&mut self) -> bool {
+        self.reg.mask.bg_enable || self.reg.mask.sprites_enable
+    }
+
+    #[inline]
+    fn render(&mut self, mem: &mut MemMap) {
+        // TODO
+        match self.scanline {
+            0..=239 => {    // rendering (visible scanlines)
+                self.render_dot(mem);
+            },
+            240 => {},      // idle (post-render scanline)
+            241..=260 => {  // vblank
+                if self.cycle == 1 && self.scanline == 241 {
+                    self.reg.status.vblank = true;
+                    debug!("enabled vblank")
+                }
+            },
+            261 => {     // dummy scanline (pre-render scanline)
+                self.rendering_fetch_data(mem); // TODO? should be blank scanline?
+
+                if self.cycle == 1 {
+                    debug!("disabled vblank");
+                    self.reg.status.vblank = false;
+                }
+
+                if self.cycle >= 280 && self.cycle <= 304 {
+                    // "Copy vertical scrolling value from t"
+                    self.reg.v = (self.reg.v & !0x7be0) | (self.reg.t & 0x7be0);
+                }
+            },
+            _ => unreachable!(),
+        }
+    }
+
+    #[inline]
+    fn render_dot(&mut self, mem: &mut MemMap) {
+        // TODO
+        // if (!self.reg.mask.bg_enable) && self.cycle >= 1 && self.cycle <= 256 {
+        //     // Rendering disabled
+        //     let x = (self.cycle - 1) as usize;
+        //     let y = self.scanline as usize;
+        //     let idx = y * PICTURE_WIDTH + x;
+
+        //     let px = as_rgb(self.pal[0]);
+
+        //     self.frame_buf.as_rgb_mut()[idx] = px;
+        // } else {
+        match self.cycle {
+            0 => {},         // idle cycle
+            1..=256 => {     // vram fetch/update
+                self.rendering_fetch_data(mem);
+
+                let x = (self.cycle - 1) as usize;
+                let y = self.scanline as usize;
+                let idx = y * PICTURE_WIDTH + x;
+                let px = self.get_next_pixel();
+
+                self.shl_shift_registers(1);
+
+                self.frame_buf.as_rgb_mut()[idx] = px;
+            },
+            257..=320 => {   // fetch tile data for sprites on the next scanline
+                if self.cycle == 257 {
+                    if self.rendering_enabled() {
+                        // "Copy horizontal scrolling value from t"
+                        self.reg.v = (self.reg.v & !0x041f) | (self.reg.t & 0x041f);
+                    }
+                }
+
+                // TODO: garbage reads?
+                // TODO2: "The shifters are reloaded during ticks 9, 17, 25, ..., 257."
+                // if self.rendering_enabled() {
+                //     self.rendering_fetch_data(mem);
+                // }
+            },
+            321..=336 => {  // fetch tile data for first two tiles for the next scanline
+                self.rendering_fetch_data(mem);
+
+                if self.rendering_enabled() {
+                    // Load the data into the registers
+                    if self.cycle == 328 || self.cycle == 336 {
+                        self.shl_shift_registers(8);
+                    }
+                }
+            },
+            337..=340 => {
+                if self.cycle == 339 && self.scanline % 2 == 1 {
+                    self.cycle = 340;
+                }
+            }, // fetch two bytes
+            _ => unreachable!(),
+        }
+        // }
+    }
+
+    fn shl_shift_registers(&mut self, amount: u16) {
+        // Update the shift register(s)
+        self.shift_reg_lo <<= amount;
+        self.shift_reg_hi <<= amount;
+    }
+
+    fn fill_shift_registers(&mut self) {
+        self.shift_reg_lo |= self.curr_pattern_lo as u16;
+        self.shift_reg_hi |= self.curr_pattern_hi as u16;
+    }
+
+    #[inline]
+    fn rendering_fetch_data(&mut self, mem: &mut MemMap) {
+        // Fetch data. Each byte takes 2 cycles
+        if !self.rendering_enabled() {
+            return;
+        }
+
+        let c = (self.cycle) % 8;
+        match c {
+            1 => {  // nametable byte
+                // "The shifters are reloaded during ticks 9, 17, 25, ..., 257."
+                self.fill_shift_registers();
+
+                // TODO? replace this with something that isn't 3 variables...
+                self.prev_attribute_byte = self.curr_attribute_byte;
+                self.curr_attribute_byte = self.temp_attribute_byte;
+
+                let addr = 0x2000 | (self.reg.v & 0x0fff);
+                self.curr_tile_idx = self.read_addr(addr, mem);
+            },
+            3 => {  // attribute table byte
+                let v = self.reg.v;
+                let addr = 0x23c0 | (v & 0x0c00) | ((v >> 4) & 0x38) | ((v >> 2) & 0x07);
+                let val = self.read_addr(addr, mem);
+
+                // Since it changes mid tile rendering, we need the pevious one too.
+                self.temp_attribute_byte = val;
+            },
+            5 | 7 => {  // pattern table tile low
+                let bg_addr = self.reg.control.bg_pattern_addr;
+                let y_scroll = self.reg.v >> 12;
+                let addr: u16 = ((self.curr_tile_idx as u16) << 4) | y_scroll | bg_addr;
+
+                if c == 5 {
+                    self.curr_pattern_lo = self.read_addr(addr, mem)
+                } else {
+                    self.curr_pattern_hi = self.read_addr(addr + 8, mem)
+                }
+            },
+            0 => {
+                self.inc_hori();
+
+                if self.cycle == 256 {
+                    self.inc_vert();
+                }
+            }
+            _ => {},
+        }
+
+        // if self.cycle == 160 && self.scanline == 48 {
+        //     let addr: u16 = ((self.curr_tile_idx as u16) << 4) | (self.reg.v >> 12) | self.reg.control.bg_pattern_addr;
+        //     println!("tile_idx: {:02x} attribute byte: {:02x}, chr_h,l: {:02x},{:02x} (addr:{:04x})\n shift: {:04x},{:04x} v: {:04x} at ({},{})",
+        //         self.curr_tile_idx,
+        //         self.curr_attribute_byte,
+        //         self.curr_pattern_hi,
+        //         self.curr_pattern_lo,
+        //         addr,
+        //         self.shift_reg_lo,
+        //         self.shift_reg_hi,
+        //         self.reg.v,
+        //         self.cycle,
+        //         self.scanline
+        //     );
+        // }
+
+        // if self.cycle == 72+8 && self.scanline == 144 {
+        //     let addr: u16 = ((self.curr_tile_idx as u16) << 4) | (self.reg.v >> 12) | self.reg.control.bg_pattern_addr;
+        //     println!("tile_idx: {:02x} attribute byte: {:02x}, chr_h,l: {:02x},{:02x} (addr:{:04x})\n shift: {:04x},{:04x} v: {:04x} at ({},{})",
+        //         self.curr_tile_idx,
+        //         self.curr_attribute_byte,
+        //         self.curr_pattern_hi,
+        //         self.curr_pattern_lo,
+        //         addr,
+        //         self.shift_reg_lo,
+        //         self.shift_reg_hi,
+        //         self.reg.v,
+        //         self.cycle,
+        //         self.scanline
+        //     );
+        // }
+    }
+
+    #[inline]
+    fn get_next_pixel(&mut self) -> RGB<u8> {
+        let scroll = self.reg.scroll_x;
+        let mut color_idx = 0;
+
+        let bg_color = ((((self.shift_reg_lo as u16) << scroll) & 0x8000) >> 15)
+                         | ((((self.shift_reg_hi as u16) << scroll) & 0x8000) >> 14);
+
+        if self.reg.mask.bg_enable {
+            color_idx = bg_color as u8;
+        }
+        let pix_val = color_idx;
+
+        // TODO: actually handle scrolling etc.
+        let tile_x = (self.cycle / 8) as usize;
+        let tile_y = (self.scanline / 8) as usize;
+
+        let tile_attr = if (scroll as u32 + (self.cycle) % 9) < 9 {
+            self.prev_attribute_byte
+        } else {
+            self.curr_attribute_byte
+        };
+
+        let pal_idx = self.pal_idx_from_attr(tile_attr, tile_x, tile_y);
+        let px = self.pixel_color(pal_idx, pix_val, false);
+
+        as_rgb(px)
+    }
+
+    /// Get the color of a specific pixel / palette pair.
+    fn pixel_color(&self, pal_idx: u8, pix_val: u8, sprite: bool) -> u8 {
+        assert!(pal_idx == (pal_idx & 0b11));
+        assert!(pix_val == (pix_val & 0b11));
+
+        let mut idx = ((sprite as u8) << 4) | (pal_idx << 2) | pix_val;
+
+        if pix_val == 0 {
+            idx &= 0b1_0000;
+        }
+
+        let mut color = self.pal[idx as usize];
+
+        if self.reg.mask.grayscale {
+            color &= 0x30;
+        }
+        color
+    }
+
     pub(crate) fn generate_nametables_image_temp(&mut self, mem: &MemMap) -> &[u8] {
         let img_w = 2 * PICTURE_WIDTH;
 
@@ -448,7 +640,7 @@ impl PPU {
 
                         let pal_idx = self.pal_idx_from_attr_table(nametable_num, nametable_x, nametable_y, mem);
 
-                        let color = self.pixel_color_at(pal_idx, pixel_color, false);
+                        let color = self.pixel_color(pal_idx, pixel_color, false);
                         let px = as_rgb(color);
 
                         self.nametable_buf.as_rgb_mut()[idx] = px;
@@ -470,6 +662,10 @@ impl PPU {
 
         let attr = self.read_addr(addr as u16, mem);
 
+        self.pal_idx_from_attr(attr, tile_x, tile_y)
+    }
+
+    fn pal_idx_from_attr(&self, attr: u8, tile_x: usize, tile_y: usize) -> u8 {
         let dx = (tile_x / 2) & 0b1;
         let dy = (tile_y / 2) & 0b1;
         let delta = 2 * ((dy << 1) | dx);
@@ -482,23 +678,43 @@ impl PPU {
         (attr & (0b11 << delta)) >> delta
     }
 
-    /// Get the color of a specific pixel / palette pair.
-    fn pixel_color_at(&self, pal_idx: u8, pix_val: u8, sprite: bool) -> u8 {
-        assert!(pal_idx == (pal_idx & 0b11));
-        assert!(pix_val == (pix_val & 0b11));
+    /// Get the index into the attribute table corresponding to the specified nametable address
+    fn pal_idx_from_attr_addr(&self, addr: u16, mem: &MemMap) -> u8 {
+        assert!(addr >= 0x2000 && addr <= 0x2fff /* && (addr & 0x0080 == 0) */, "Addr: {:04x}", addr);
+        let tile_x = (addr & 0xfff) % 32;
+        let tile_y = (addr & 0xfff) >> 5;
+        let nametable_num = (addr & 0xc00) >> 10;
 
-        let mut idx = ((sprite as u8) << 4) | (pal_idx << 2) | pix_val;
+        self.pal_idx_from_attr_table(nametable_num as usize, tile_x as usize, tile_y as usize, mem)
+    }
 
-        if pix_val == 0 {
-            idx &= 0b1_0000;
+    fn inc_hori(&mut self) -> () {
+        if (self.reg.v & 0x1f) == 31 {  // if coarse X == 31
+            self.reg.v &= !0x001f;      // coarse X = 0
+            self.reg.v ^= 0x0400;       // switch horizontal nametable
+        } else {
+            self.reg.v += 1             // increment coarse X
         }
+    }
 
-        let mut color = self.pal[idx as usize];
-
-        if self.reg.mask.grayscale {
-            color &= 0x30;
+    fn inc_vert(&mut self) -> () {
+        let mut v = self.reg.v;
+        if (v & 0x7000) != 0x7000 {             // if fine Y < 7
+            v += 0x1000                         // increment fine Y
+        } else {
+            v &= !0x7000;                       // fine Y = 0
+            let mut y = (v & 0x03E0) >> 5; // let y = coarse Y
+            if y == 29 {
+                y = 0;                          // coarse Y = 0
+                v ^= 0x0800;                    // switch vertical nametable
+            } else if y == 31 {
+                y = 0                           // coarse Y = 0, nametable not switched
+            } else {
+                y += 1                          // increment coarse Y
+            }
+            v = (v & !0x03E0) | (y << 5)        // put coarse Y back into v
         }
-        color
+        self.reg.v = v;
     }
 }
 
