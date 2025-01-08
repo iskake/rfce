b15d1faa46198ee976751bcbbfed019ff5680464
iskake <iskake@protonmail.com>
2025-01-08 17:47:05 +0100

    Correct handling of command line args

--- src/main.rs
+++ src/main.rs
@@ -14,26 +14,38 @@ fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
 
     let headless = args.contains(&"--headless".to_owned());
+    let last_arg_is_nes_file = args[args.len() -1].ends_with(".nes");
 
-    if let Some(filename) = args.get(args.len() - 1) {
-        if headless {
+    if headless {
+        if last_arg_is_nes_file {
             info!("Starting headless debugger");
 
+            let filename = &args[args.len() - 1];
             let mut debugger = Debugger::new();
-            match debugger.load_file(Path::new(filename)) {
+            match debugger.load_file(Path::new(&filename)) {
                 Ok(_) => Ok(debugger.run()),
-                Err(e) => Err(format!("{} (filename: '{}')", e, filename)),
+                Err(e) => Err(format!(
+                    "{} (file: '{}') Help: Did you specify a valid .nes file?",
+                    e, filename
+                )),
             }
         } else {
-            info!("Starting GUI");
-
-            let mut gui = GUI::from_file(Path::new(filename));
-            gui.run_forever();
+            println!("No nes file provided.\n");
+            println!("Usage: rfce --headless <file>");
             Ok(())
         }
     } else {
-        println!("No nes file provided.\n");
-        println!("Usage: rfce [options] <file>");
+        info!("Starting GUI");
+
+        let mut gui = if last_arg_is_nes_file {
+            let filename = &args[args.len() - 1];
+            info!("Loading .nes file: {filename}");
+            GUI::from_file(Path::new(filename))
+        } else {
+            info!("Starting without ROM");
+            GUI::new()
+        };
+        gui.run_forever();
         Ok(())
     }
 }
